2110f2a9b6ed7baf5632644775506e484b1bf4f1
Andrej Bartulin <andrej.bartulinvr@gmail.com>
2023-11-12 21:36:18 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# TransitCard
+
+Paper plugin for transit cards in Minecraft
+
+Version: 1.19.4+
+
+## How to use
+- Run /metro_card or any similar command
+
+### Permissions
+- `transit_card.buy.metro`
+- `transit_card.buy.monorail`
+
+Or simply: `transit_card.buy.transit`
+
+### Placeholders:
+- none at this moment
+
+### Config
+
+Just read comments
+
+```yml
+# Do you want to give piece of paper to players
+electronic_card: true
+
+# Price of metro card
+metro_card_price: 1
+
+# Price of monorail card
+monorail_card_price: 1
+```
+
+## How to compile
+- Download `Minecraft Development` plugin in `IntelliJ IDEA`
+- Build artifact, don't forget to modify path
+
+[Full video tutorial (you won't need everything)](https://www.youtube.com/watch?v=5DBJcz0ceaw)
+
+*You can also just download `exec-signs.jar` file*
---
+++ src/main/java/me/andrej123456789/transitcard/commands/Help.java
@@ -0,0 +1,36 @@
+package me.andrej123456789.transitcard.commands;
+
+import me.andrej123456789.transitcard.TransitCard;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Help implements CommandExecutor, TabExecutor {
+
+    private static final Plugin plugin = JavaPlugin.getProvidingPlugin(TransitCard.class);
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        sender.sendMessage("GitHub: " + ChatColor.AQUA + "\n" + ChatColor.RESET);
+
+        sender.sendMessage("/metro_card [hours] - buy metro card (if hours argument is not passed, card is valid for 1 hour)");
+        sender.sendMessage("/monorail_card [hours] - buy monorail card");
+
+        return true;
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        return new ArrayList<>(); /* null = all player names */
+    }
+}
